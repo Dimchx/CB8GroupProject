56a774f4a0ebc8a753d6f89086344e71b99b98cb
jenkei <i.kontinopoulos@protonmail.com>
2019-07-09 13:28:46 +0300

    Create .NET Core Anew

---
+++ CB8_TeamYBD_GroupProject_MVC/CB8_TeamYBD_GroupProject_MVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CB8_TeamYBD_GroupProject_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
