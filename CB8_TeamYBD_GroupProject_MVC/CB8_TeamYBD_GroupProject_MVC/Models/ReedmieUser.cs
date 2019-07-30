6de81b5e64a6f00893db40a5dfc113d6a5995ffc
jenkei <i.kontinopoulos@protonmail.com>
2019-07-30 11:03:24 +0300

    ReedmieUser

--- CB8_TeamYBD_GroupProject_MVC/CB8_TeamYBD_GroupProject_MVC/Models/ReedmieUser.cs
+++ CB8_TeamYBD_GroupProject_MVC/CB8_TeamYBD_GroupProject_MVC/Models/ReedmieUser.cs
@@ -8,6 +8,13 @@ namespace CB8_TeamYBD_GroupProject_MVC.Models
 {
     public class ReedmieUser:IdentityUser
     {
+        public virtual ICollection<Article> AuthoredArticles { get; set; }
+        public virtual ICollection<Article> LikedArticles { get; set; }
+        public virtual ICollection<Article> PurchasedArticles { get; set; }
+        public virtual ICollection<ReedmieUser> Following { get; set; }
+        public virtual ICollection<ReedmieUser> Followers { get; set; }
+        public virtual ICollection<ReedmieUser> SubscribedTo { get; set; }
+        public virtual ICollection<ReedmieUser> Subscribers { get; set; }
 
     }
 }
