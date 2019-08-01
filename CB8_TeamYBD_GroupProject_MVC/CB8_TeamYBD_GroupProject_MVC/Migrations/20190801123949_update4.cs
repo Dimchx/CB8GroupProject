e978ca0e152ab95c449e0433f300032973f52694
jenkei <i.kontinopoulos@protonmail.com>
2019-08-01 15:40:11 +0300

    migration

---
+++ CB8_TeamYBD_GroupProject_MVC/CB8_TeamYBD_GroupProject_MVC/Migrations/20190801123949_update4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CB8_TeamYBD_GroupProject_MVC.Migrations
+{
+    public partial class update4 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "LikeDateTime",
+                table: "CommentLikes",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "LikeDateTime",
+                table: "CommentLikes");
+        }
+    }
+}
