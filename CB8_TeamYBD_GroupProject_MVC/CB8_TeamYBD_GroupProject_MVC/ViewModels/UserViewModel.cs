7ca9dc964bc3ba45ca88dd81e4e0f321a525fb9c
jenkei <i.kontinopoulos@protonmail.com>
2019-08-06 10:08:28 +0300

    user details

---
+++ CB8_TeamYBD_GroupProject_MVC/CB8_TeamYBD_GroupProject_MVC/ViewModels/UserViewModel.cs
@@ -0,0 +1,16 @@
+﻿using CB8_TeamYBD_GroupProject_MVC.Areas.Identity.Data;
+using CB8_TeamYBD_GroupProject_MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CB8_TeamYBD_GroupProject_MVC.ViewModels
+{
+    public class UserViewModel
+    {
+        public CB8_TeamYBD_GroupProject_MVCUser User { get; set; }
+        public List<Article> Articles { get; set; }
+        public List<SubscriptionListing> listings { get; set; }
+    }
+}
